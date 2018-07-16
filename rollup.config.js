463e0f42567cfe741c0f0fb46688ababaaacf718
Misael Bitencourt <misael.bitencourt@santaluzia.corp>
2018-07-16 08:53:53 -0300

    adds steps to build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,17 @@
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+
 export default {
     entry: 'src/index.js',
     output: {
         file: 'dist/select-image.js',
         format: 'iife',
         name: 'selectImage'        
-    }    
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+        }),
+        uglify()
+    ],
 }
